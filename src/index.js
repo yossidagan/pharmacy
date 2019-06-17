6c62a6a47090463519d19b411bac535c24b86c3b
yossiD <dagangm@gmail.com>
2019-06-17 12:26:27 +0300

    basic setup

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { Provider } from 'mobx-react'
+import * as serviceWorker from './serviceWorker';
+import { GeneralStore } from './GeneralStore';
+
+
+const generalStore = new GeneralStore()
+
+
+const stores = { generalStore }
+
+ReactDOM.render(<Provider {...stores}>
+    <App />
+</Provider>, document.getElementById('root'));
+
+serviceWorker.register();
