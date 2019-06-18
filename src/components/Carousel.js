89fa7897bd6d5fb1258076a9d64d580eaf3ff3ca
yossiD <dagangm@gmail.com>
2019-06-18 18:24:03 +0300

    updated navbar

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -2,12 +2,20 @@ import React, { Component } from "react";
 import M from "materialize-css";
 import "materialize-css/dist/css/materialize.min.css";
 import { observer, inject } from 'mobx-react';
+import CartModal from "./CartModal";
 // import data from "../data.json"
 
 @inject("generalStore")
 @observer
 
 class Carousel extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      showModal: false
+    }
+  }
   componentDidMount() {
     const options = {
       duration: 300,
@@ -18,8 +26,8 @@ class Carousel extends Component {
     M.Carousel.init(this.Carousel, options);
   }
 
-  showModal = () => console.log("Ok")
-  
+  showModal = () => this.setState({ showModal: !this.state.showModal })
+
   renderThis = () => {
 
     // const data = this.props.generalStore.products
@@ -28,29 +36,35 @@ class Carousel extends Component {
       {
         name: "hamburger",
         pic: "https://assets.epicurious.com/photos/57c5c6d9cf9e9ad43de2d96e/6:4/w_620%2Ch_413/the-ultimate-hamburger.jpg"
-    }, {
-      name: "deserts",
-      pic: "https://cdn.apartmenttherapy.info/image/fetch/f_auto,q_auto:eco,c_fill,g_auto,w_760/https://storage.googleapis.com/gen-atmedia/3/2018/12/8cfbcbb2919742682345681d469b7417a73e4dfe.jpeg"
-  }
+      }, {
+        name: "deserts",
+        pic: "https://cdn.apartmenttherapy.info/image/fetch/f_auto,q_auto:eco,c_fill,g_auto,w_760/https://storage.googleapis.com/gen-atmedia/3/2018/12/8cfbcbb2919742682345681d469b7417a73e4dfe.jpeg"
+      }
     ]
 
     return data.map(i => (
       <a key={i.name} className="carousel-item">
-        <img onClick={this.showModal} src={i.pic} /> 
+        <img onClick={this.showModal} src={i.pic} />
       </a>
     ));
   };
   render() {
     return (
-      <div
-        ref={Carousel => {
-          this.Carousel = Carousel;
-        }}
-        className="carousel"
-      >
-        {this.renderThis()}
+      <div>
+        <div
+          ref={Carousel => {
+            this.Carousel = Carousel;
+          }}
+          className="carousel"
+        >
+          {this.renderThis()}
+        </div>
+
+        {/* {this.showModal ? <CartModal /> : null} */}
+
       </div>
-    );
+
+    )
   }
 }
 
